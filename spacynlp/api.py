c3588f8acedf37c223a7f35fc0e6faeebab4d469
jueheed <jueheenara@gmail.com>
2020-04-28 15:54:57 -0400

    Updated api.py with rest api to process text

--- spacynlp/api.py
+++ spacynlp/api.py
@@ -1,13 +1,75 @@
-import flask
-from flask import request, jsonify
+from flask import Flask, request, jsonify, render_template, url_for, request
+import re
+import pandas as pd
 from default_ner_example import DefaultNerEx
+import spacy
+from spacy import displacy
+from cgitb import text
 
-app = flask.Flask(__name__)
+dataFile = 'data/data1.txt'
+nlp = spacy.load('en_core_web_lg')
+
+app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-dataFile = 'data/data1.txt'
 
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/process', methods=["POST"])
+def process():
+    if request.method == 'POST':
+        choice = request.form['taskoption']
+        rawtext = request.form['rawtext']
+        doc = nlp(rawtext)
+        d = []
+        for ent in doc.ents:
+            d.append((ent.label_, ent.text))
+            df = pd.DataFrame(d, columns=('named entity', 'output'))
+            ORG_named_entity = df.loc[df['named entity'] == 'ORG']['output']
+            PERSON_named_entity = df.loc[df['named entity'] == 'PERSON']['output']
+            GPE_named_entity = df.loc[df['named entity'] == 'GPE']['output']
+            MONEY_named_entity = df.loc[df['named entity'] == 'MONEY']['output']
+            DATE_named_entity = df.loc[df['named entity'] == 'DATE']['output']
+            CARDINAL_named_entity = df.loc[df['named entity'] == 'CARDINAL']['output']
+        if choice == 'organization':
+            results = ORG_named_entity
+            num_of_results = len(results)
+        elif choice == 'person':
+            results = PERSON_named_entity
+            num_of_results = len(results)
+        elif choice == 'geopolitical':
+            results = GPE_named_entity
+            num_of_results = len(results)
+        elif choice == 'money':
+            results = MONEY_named_entity
+            num_of_results = len(results) 
+        elif choice == 'date':
+            results = DATE_named_entity
+            num_of_results = len(results) 
+        elif choice == 'cardinal':
+            results = CARDINAL_named_entity
+            num_of_results = len(results)
+             
+    return render_template("index.html", results=results, num_of_results = num_of_results)  
+
+""" 
+    Return jsonified entity list given text
+"""         
+@app.route('/api/ner', methods = ['GET']) 
+def get_all_entities_from_msg():
+    #text = request.args.get('text', '')
+    text = request.args.get('text')
+    doc = nlp(text)
+    d = []
+    for ent in doc.ents:
+        d.append((ent.text, ent.label_))
+            
+    return jsonify(d)
+    
+           
 # A route to return all of the available entries from NER search.
 @app.route('/api/v1/resources/entities/all', methods=['GET'])
 def get_all_entities():
@@ -19,7 +81,7 @@ def get_all_entities():
            ent1.append(str(ent[0]))
            ent1.append(ent[1])
            entity_list.append(dict(zip(keys,ent1)))
-
+           
     return jsonify(entity_list)
 
 # A route to return all of the available entries from NER search.

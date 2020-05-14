1a4782dd5407c93bc20f3e9b76b22ffee4b9b566
Hong Zheng <hzheng63@yahoo.com>
2020-05-14 14:53:53 -0400

    updated spacy's default ner model or custom model

--- spacynlp/readme.md
+++ spacynlp/readme.md
@@ -6,11 +6,20 @@ Eclipse.  This project is pure python based.
 4. put sample data into data folder in the project.
 5. run the python file directly inside eclipse.
 
-
 Note: sample data is not included here which can be downloaded from wiki page provided.
-      sample data for train model can use sample 1, then each test case there meet 100% expectation.
+      sample data for train model can use sample 1, then each test case there meet expectation.
 
-I also create a simple prototype rest API (api.py) to test the functonality based on default model
+There are the following ways to run train ner script
+1. if update an existing default model and then do a NER search
+      run TrainNerEx().ner_search('data/data1.txt','en_core_web_lg','y')
+2. if update an existing custom model and then do a NER search
+      run TrainNerEx().ner_search('data/data1.txt','custommodel','y')
+3. if only do a NER search against an existing custom model without creating or updating any model
+      run TrainNerEx().ner_search('data/data1.txt')
+4. if create a new custom model from blank and then do a NER search
+      run TrainNerEx().ner_search('data/data1.txt',is_train='y')
+
+A prototype rest API (api.py) is also created to test the functonality based on default model
 as following:
 1. pip out flask library
 2. python api.py
@@ -19,7 +28,7 @@ as following:
     'http://localhost:5000/api/v1/resources/entities/all?lang=fr' if text body is french 
     'http://localhost:5000/api/v1/resources/entities/all?label=DATE' if need to do a category search
 
-   notice: Content-Type should be "text/plain" in the header. By default url, it is english content. Otherwise, attach query parameter lang=fr to url for french content.
+Note: Content-Type should be "text/plain" in the header. By default url, it is english content. Otherwise, attach query parameter lang=fr to url for french content.
       
 You can also enter text input to view NER results
 To view visually, select 'default' option
--- spacynlp/train_ner_example.py
+++ spacynlp/train_ner_example.py
@@ -5,26 +5,45 @@ Created on Mar. 26, 2020
 '''
 import spacy
 import random
+import json
+import ast
 from pprint import pprint
+from spacy.util import minibatch, compounding
+from pathlib import Path
+import shutil
+from spacy.util import decaying
 
 
 class TrainNerEx:
-   #TRAIN_DATA= [('I am writing to report a deal which has aroused my suspicion who does a money laundering.', {'entities': [(72, 88, 'KW')]}), ('The fraudulent from the release of the deposit would be handled by the lawyers.', {'entities': [(4, 14, 'KW')]}), ('The fraud was also investigated by police but there is no result.', {'entities': [(4, 9, 'KW'), (35, 41, 'KW')]})]
-   TRAIN_DATA= [('Introduction:Internal Case #C92646759\nFinTRAC Reference Number: 348729667', {'entities': [(38, 45, 'ORG')]}), ('On 1/12/2017 Capital One Bank USA, NA (COBUSANA) detected patterns of activity on credit card account number 1234567890123456', {'entities': [(3, 12, 'DATE'), (13, 33, 'ORG'), (35, 37, 'GPE')]}), ('Capital One has identified an unknown suspect', {'entities': [(0, 11, 'ORG')]}), ('All supporting documentation is maintained by Capital One Bank (USA) N.A.', {'entities': [(46, 62, 'ORG')]}), ('The suspicious activity was conducted in Canada with Walmart, restaurants, Target, Quality Inn, Best Buy and other brick and mortar merchants', {'entities': [(41, 47, 'GPE'), (53, 60, 'ORG'), (75, 81, 'ORG'), (83, 94, 'ORG'), (96, 104, 'ORG')]}), ('From 3/15/2016 to 5/14/2016 suspicious activity occurred totaling $8,098.76', {'entities': [(5, 14, 'DATE'), (18, 27, 'DATE'), (66, 75, 'MONEY')]}), ('The sample is drawn from transaction activity occurring from 5/12/2016 to 5/14/2016', {'entities': [(61, 70, 'DATE'), (74, 83, 'DATE')]}), ('Included in Section B of this report is a sampling of transactional activity totaling $30.0', {'entities': [(86, 91, 'MONEY')]})]
-
-   def train_spacy(self, data, iters):
+   #TRAIN_DATA=[] #move the train data to a file already
+   
+   def __init__(self):
+       self.model_file = 'custommodel'
+       self.train_data_file = 'data/train-data.txt'
+       self.dropout = decaying(0.1, 0.0, 1e-4)
+       self.iters=20
+       
+   def train_model(self, data, iters, model):
 
           TRAIN_DATA = data
-          # create blank Language class
-          nlp = spacy.blank('en')
+          if model is not None:
+             #load existing model
+             nlp=spacy.load(model)
+             print("Loaded model '%s'" % model)
+          else:
+             # create blank Language class
+             nlp = spacy.blank('en')
+             print("Created blank 'en' model")
     
           # create the built-in pipeline components and add them to the pipeline
           # nlp.create_pipe works for built-ins that are registered with spaCy
           if 'ner' not in nlp.pipe_names:
               ner = nlp.create_pipe('ner')
               nlp.add_pipe(ner, last=True)
+          else:
+              ner = nlp.get_pipe('ner')
        
-          # add labels
+          # add new labels
           for _, annotations in TRAIN_DATA:
                for ent in annotations.get('entities'):
                   ner.add_label(ent[2])
@@ -32,49 +51,72 @@ class TrainNerEx:
           # get names of other pipes to disable them during training
           other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
           with nlp.disable_pipes(*other_pipes):  # only train NER
-              optimizer = nlp.begin_training()
+              if model is None:
+                 # initialize the weights randomly only if training a new model
+                 optimizer = nlp.begin_training()
+              else:
+                 optimizer = nlp.resume_training() #nlp.entity.create_optimizer()
+                  
               for iter in range(iters):
                   print("Starting iteration " + str(iter))
                   random.shuffle(TRAIN_DATA)
                   losses = {}
-                  for text, annotations in TRAIN_DATA:
-                      nlp.update(
-                          [text],  # batch of texts
-                          [annotations],  # batch of annotations
-                          drop=0.2,  # dropout - make it harder to memorise data
-                          sgd=optimizer,  # callable to update weights
-                          losses=losses)
-                  print(losses)
+                  batches = minibatch(TRAIN_DATA, size=compounding(1., 4., 1.001))
+                  for batch in batches:
+                      texts, annotations = zip(*batch)
+                      
+                      # Updating the weights
+                      nlp.update(texts,
+                                 annotations,
+                                 sgd=optimizer,  # callable to update weights
+                                 drop=0.0,  # dropout - make it harder to memorise data
+                                 losses=losses)
+                  print("Losses", losses)
           return nlp
       
-   def ner_search(self,file):
-      # Read whole documents
-      f=open(file, "r")
-      message=f.read()
-      TRAIN_DATA = self.TRAIN_DATA
-      nlp =  self.train_spacy(TRAIN_DATA,20)
+   def ner_search(self, file, model=None, is_train=None):
+      # Read whole raw text
+      with open(file,'r') as f:
+         message = f.read()
+      
+      with open(self.train_data_file,"r") as tdf:
+         TRAIN_DATA = tdf.read()
 
+      TRAIN_DATA = ast.literal_eval(' '.join(TRAIN_DATA.split()))
+      #TRAIN_DATA = self.TRAIN_DATA
 
-      model_file = 'custommodel'
-      # Save our trained Model
-      nlp.to_disk(model_file)
-
-      #Test each sentence from train data
-      for text, _ in TRAIN_DATA:
-          nlp(text)
-
-      nlp = spacy.load(model_file)
+      if is_train is not None:
+         nlp =  self.train_model(TRAIN_DATA, self.iters, model)
+         
+         model_file_dir = Path(self.model_file)
+         if model_file_dir.exists() and model_file_dir.is_dir() and self.model_file is not model:
+            shutil.rmtree(model_file_dir)
+         # Save our trained Model
+         nlp.to_disk(self.model_file)
+         #Test each sentence from train data
+         for text, _ in TRAIN_DATA:
+             nlp(text)
+             
+      if self.model_file is not None:
+        model_file_dir = Path(self.model_file)
+        if not model_file_dir.exists():
+            print("Created a custom model first!")
+            return []
+      nlp = spacy.load(self.model_file)
       doc = nlp(message)
 
       entities=[]
       for ent in doc.ents:
           entities.append((ent.text, ent.label_))
       return entities
-      
+       
 
       
 if __name__ == '__main__':
-   entities=TrainNerEx().ner_search('data/data1.txt')
+   entities=TrainNerEx().ner_search('data/data1.txt') # run against the existing model: custommodel
+   #entities=TrainNerEx().ner_search('data/data1.txt',is_train='y') #run against the custommodel created just from blank
+   #entities=TrainNerEx().ner_search('data/data1.txt','custommodel','y') # run against the existing custommodel just updated
+   #entities=TrainNerEx().ner_search('data/data1.txt','en_core_web_lg','y') # run against the custommodel created just from en_core_web_lg with new update
    
    # Find named entities from the message
    pprint([(entity) for entity in entities])
